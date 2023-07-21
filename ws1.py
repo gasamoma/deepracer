7e9a1902738956f8ab42a7c2b075e9c317cf3322
Gabriel Santiago Montero Mariño <gabriel_montero@hotmail.com>
2023-07-21 08:21:45 -0600

    Create ws1.py

---
+++ ws1.py
@@ -0,0 +1,40 @@
+def reward_function(params):
+    '''
+    Example of rewarding the agent to follow center line
+    '''
+    
+    # Read input parameters
+    track_width = params['track_width']
+    distance_from_center = params['distance_from_center']
+    all_wheels_on_track = params['all_wheels_on_track']
+    speed = params['speed']
+    SPEED_THRESHOLD = 1.4 
+    abs_steering = abs(params['steering_angle']) # Only need the absolute steering angle
+    
+    # Calculate 3 markers that are at varying distances away from the center line
+    marker_1 = 0.1 * track_width
+    marker_2 = 0.25 * track_width
+    marker_3 = 0.5 * track_width
+    
+    # Give higher reward if the car is closer to center line and vice versa
+    if distance_from_center <= marker_1:
+        reward = 1.0
+    elif distance_from_center <= marker_2:
+        reward = 0.5
+    elif distance_from_center <= marker_3:
+        reward = 0.1
+    else:
+        reward = 1e-3  # likely crashed/ close to off track
+
+    ABS_STEERING_THRESHOLD = 15 
+    
+    # Penalize reward if the car is steering too much
+    if abs_steering > ABS_STEERING_THRESHOLD:
+        reward *= 0.8
+    
+    if not all_wheels_on_track:		
+        reward = 1e-3  # Penalize if the car goes off track
+    elif speed < SPEED_THRESHOLD:		
+        reward *= 0.5  # Penalize if the car goes too slow
+  
+    return float(reward)
